30e86788479a6f92f86f0ed0be53a2b2ea3d2647
Chris Prince <chrisprince177@gmail.com>
2026-01-04 03:14:12 -0800

    Save submitted form data to localStorage

--- script.js
+++ script.js
@@ -18,3 +18,18 @@ document.getElementById("addField").addEventListener("click", function () {
   extraInput.placeholder = "Extra Field";
   form.insertBefore(extraInput, form.querySelector("button[type='submit']"));
 });
+document.getElementById("registerForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let course = document.getElementById("course").value;
+
+  localStorage.setItem("studentName", name);
+  localStorage.setItem("studentEmail", email);
+  localStorage.setItem("studentPhone", phone);
+  localStorage.setItem("studentCourse", course);
+
+  alert("Student data saved locally!");
+});
